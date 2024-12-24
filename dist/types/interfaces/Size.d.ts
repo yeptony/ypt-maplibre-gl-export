65c3e35d50426f24dc92428a338707dd194d41db
Tom <tom@tgprod.dev>
2024-12-24 10:42:19 +0100

    fix

--- dist/types/interfaces/Size.d.ts
+++ dist/types/interfaces/Size.d.ts
@@ -2,8 +2,8 @@ export declare const Size: {
     readonly LARGE: readonly [474, 406.2];
     readonly LANDSCAPE: readonly [609.4, 304.65];
     readonly EXTRA: readonly [507.75, 406.2];
-    readonly A_FORMAT: readonly [440, 14, 406.2];
-    readonly A_FORMAT_LANDSCAPE: readonly [574.55, 175.07];
+    readonly A_FORMAT: readonly [440.14, 406.2];
+    readonly A_FORMAT_LANDSCAPE: readonly [574.55, 304.71];
     readonly A4: readonly [297, 210];
 };
 export type SizeType = (typeof Size)[keyof typeof Size];
